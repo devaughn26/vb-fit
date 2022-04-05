39a812d48a6ced06a5e9ab90c39272eac43e8c5d
devaughn26 <devaughnmartiny2k@hotmail.com>
2022-04-05 17:51:08 +0000

    linking heroku db

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://uukyubdpmvmpwz:c6f34d56f51b88d48883685ec5246703729dee79aca0aae842e7c8efc63701e7@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d7ah93sca54t6g"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
